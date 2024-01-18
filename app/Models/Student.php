9d9151c570e1446a4c98041e07da1a7a30f6afca
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2024-01-19 00:08:30 +0200

    studnet controller and model

---
+++ app/Models/Student.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Student extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'ph_number',
+        'address'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function exams()
+    {
+        return $this->belongsToMany(Exam::class, 'user_exam', 'user_id');
+    }
+}
